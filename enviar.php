0a37bdd941683608eb7e0f9b2584460f481eca8b
Nahu1500 <59622958+Nahu1500@users.noreply.github.com>
2020-04-28 18:12:50 -0300

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -13,7 +13,7 @@ $mensaje .= "Su e-mail es: " . $mail . " \r\n";
 $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'nahuel.ezequiel.lopezferme@gmail.com';
+$para = 'brankoh@gmail.com';
 $asunto = $_POST['contact-subject'];
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
